355f281e452602ab5cc49db4d2fd6ff2312fe162
rgaudin <reg@rskg.org>
2026-03-05 16:41:10 +0000

    Fixed get_digest test
    That particular test was returning an unexpected digest at time
    the test was written. A comment attest this.
    It is still returning a different value than the one shown in
    docker hub.

--- tests/test_digest.py
+++ tests/test_digest.py
@@ -19,9 +19,10 @@ kiwix_tools: str = "ghcr.io/kiwix/kiwix-tools"
         (
             "helloysd/caddy:0.10.11",
             "linux/amd64",
-            "672342e30b241ae37f442b0afdd6e744f776af3bd602c4bceaa8dcf2547841bd",
-            # different from hub (v1 manifest)
-            # "sha256:8a7b91584f5d0ee6211249d05ec51f026b763d3e1a87885e7d0d6968c42ad6b1",
+            # on docker hub digest is shown as
+            # "sha256:8a7b91584f5d0ee6211249d05ec51f026b763d3e1a87885e7d0d6968c42ad6b1"
+            # which is the Docker-Content-Digest
+            "sha256:148e55d6f2c3fa74bdba8f2b6870677cfcb268e5a9bebc9e5135a026f502f447",
         ),
         (
             f"{kiwix_tools}:3.5.0-2",
